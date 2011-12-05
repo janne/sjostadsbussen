8753d75bfc627c8da222cda5b09d31816caba869
Jan Andersson <jan.andersson@mynewsdesk.com>
2011-12-05 16:15:09 +0100

    Added url /departures.json that fetches departures from trafiklab
    Requires environment variable API_KEY to be set

--- app.rb
+++ app.rb
@@ -1,7 +1,18 @@
 require 'sinatra'
+require 'coffee-script'
+require 'patron'
 
 get '/' do
   send_file File.join('public', 'index.html')
+end
+
+get '/departures.json' do
+  sess = Patron::Session.new
+  siteId = 1365
+  key = ENV['API_KEY']
+  sess.base_url = "https://api.trafiklab.se/"
+  response = sess.get "/sl/realtid/GetDpsDepartures.json?key=#{key}&siteId=#{siteId}"
+  response.body
 end
 
 get '/javascripts/application.js' do
