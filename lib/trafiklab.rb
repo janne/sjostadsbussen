bddfb44a7647ca7a84594612cecedb6382c7a3ba
Jan Andersson <jan.andersson@mynewsdesk.com>
2011-12-06 20:46:58 +0100

    Move line_number param to Trafiklab#departures

--- lib/trafiklab.rb
+++ lib/trafiklab.rb
@@ -2,16 +2,15 @@ require 'patron'
 require 'json'
 
 class Trafiklab
-  def initialize(api_key, line_number = 74)
+  def initialize(api_key)
     @api_key = api_key or raise "API_KEY is missing"
     @sess = Patron::Session.new
     @sess.base_url = "https://api.trafiklab.se/"
-    @line_number = line_number
   end
 
-  def departures(site_id)
+  def departures(site_id, line_number = 74)
     response = @sess.get "/sl/realtid/GetDpsDepartures.json?key=#{@api_key}&siteId=#{site_id}&timeWindow=60"
     deps = JSON.parse(response.body)
-    deps["DPS"]["Buses"]["DpsBus"].select{|s| s['LineNumber'] == @line_number}
+    deps["DPS"]["Buses"]["DpsBus"].select{|s| s['LineNumber'] == line_number}
   end
 end
