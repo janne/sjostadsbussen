ac96c0be9c8343f09b389f91531c24d259b301cf
Jan Andersson <jan.andersson@mynewsdesk.com>
2012-01-23 15:47:05 +0100

    Added basic functionality with Ember.js

---
+++ public/javascripts/application.js
@@ -0,0 +1,22 @@
+window.App = Ember.Application.create();
+
+Traffic = Ember.Object.extend({
+    sites: {
+        1365: 'S&ouml;dra Station',
+        1366: 'Wollmar Yxkullsg',
+        1552: 'Luma',
+        4633: 'Sickla Strand',
+        9820: 'Sickla Udde'
+    },
+    currentSiteId: function() { return this.get('isAfternoon') ? 1366 : 1552 }.property('isAfternoon'),
+    currentSite: function() { return this.get('sites')[this.get('currentSiteId')] }.property('sites', 'currentSiteId'),
+    isAfternoon: function() { return (new Date()).getHours() >= 12 }.property(),
+    departures: [],
+    refresh: function() {
+        $.getJSON("/departures/" + this.get('currentSiteId') + ".json", function(data) { App.traffic.set('departures', data) });
+    }
+});
+
+// Setup
+App.traffic = Traffic.create();
+App.traffic.refresh();
