2bbe87a35ffe6a3a2a26d226ee38d25bf326fe08
Jan Andersson <jan.andersson@mynewsdesk.com>
2012-01-23 11:01:41 +0100

    Use unicorn with four procs

--- app.rb
+++ app.rb
@@ -3,16 +3,18 @@ require 'coffee-script'
 require 'yaml'
 require './lib/trafiklab'
 
-get '/' do
-  send_file File.join('public', 'index.html')
-end
+class App < Sinatra::Application
+  get '/' do
+    send_file File.join('public', 'index.html')
+  end
 
-get '/departures/:site_id.json' do
-  content_type 'application/json'
-  trafiklab = Trafiklab.new(ENV['API_KEY'] || YAML.load_file('config/config.yml')['API_KEY'])
-  trafiklab.departures(params[:site_id]).to_json
-end
+  get '/departures/:site_id.json' do
+    content_type 'application/json'
+    trafiklab = Trafiklab.new(ENV['API_KEY'] || YAML.load_file('config/config.yml')['API_KEY'])
+    trafiklab.departures(params[:site_id]).to_json
+  end
 
-get '/javascripts/application.js' do
-  coffee :application
+  get '/javascripts/application.js' do
+    coffee :application
+  end
 end
